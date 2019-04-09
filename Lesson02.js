ae2aeb7ceb319e3c540769676b0fd442a6757887
chad704 <35078624+chad704@users.noreply.github.com>
2019-04-09 19:21:33 -0400

    Add files via upload
    Lesson 02-Data Structures Arrays & Objects

---
+++ Lesson02.js
@@ -0,0 +1,31 @@
+//Exercise 1
+
+/*let list = ['pop tarts', 'ramen noodles', 'chips', 'salsa', 'coffee'];
+
+list.push('fruit loops');
+list[4] = 'fair trade coffee';
+list.splice(2,2,'rice','beans');
+var cart = [];
+cart.unshift(list.pop());
+cart.push(list.shift());
+
+while (list.length > 0) {
+    cart.push(list.pop());
+};
+
+cart.reverse(cart.sort());
+cart.toString(cart.join(','));
+
+console.log(cart);
+console.log(list);
+
+//Exercise 2 Addressing Objects
+
+1. course.name = 'Javascript Applications'
+2. course.teachers[1] = 'Shane'
+3. course.students[0].name = 'Steve'
+4. course.students[1].computer.type = 'macbook'
+5. course.preReqs.equipment
+6. course.preReqs.equipment.OSOptions[1] = 'osx'
+7. course.preReqs.equipment.OSOptions.join(' or ');*/
+8. const studentsOSX = [course.students[1].name,course.students[2].name];
